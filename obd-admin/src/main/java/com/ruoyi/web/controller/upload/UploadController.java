793824f7777db10346cfd146ec3d37ffa3f89468
hongkunfeng <793888681@qq.com>
2020-09-28 17:41:19 +0800

    新增图片上传文件夹

---
+++ obd-admin/src/main/java/com/ruoyi/web/controller/upload/UploadController.java
@@ -0,0 +1,17 @@
+package com.ruoyi.web.controller.upload;
+
+import com.ruoyi.common.core.controller.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 图片上传
+ * 
+ * @author
+ */
+@Controller
+@RequestMapping("/upload")
+public class UploadController extends BaseController
+{
+
+}
